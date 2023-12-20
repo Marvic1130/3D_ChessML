3a72d4cb59090c44e6f03b8f041d963a177674aa
Marvic1130 <marvic1130@gmail.com>
2023-12-20 19:02:42 +0900

    no message

--- app.py
+++ app.py
@@ -1,70 +1,48 @@
 from flask import Flask, jsonify, request
-import socket
 import threading
+from websocket_server import WebsocketServer
 import time
 
 app = Flask(__name__)
-servers = {}  # Dictionary to track active servers
-lock = threading.Lock()  # Thread lock for managing access to the servers dictionary
+servers = {}  # 서버 인스턴스를 추적하는 딕셔너리
 
-def socket_server_thread(server_socket, host, port):
-    with lock:
-        servers[(host, port)] = {"server": server_socket, "active": True}
-    print(f"Socket listening on {host} port {port}")
+def new_client(client, server):
+    print(f"Connected new socket: {client['address']}")
 
-    while servers[(host, port)]["active"]:
-        try:
-            client_socket, addr = server_socket.accept()
-            print(f"Connected by {addr}")
-            while True:
-                data = client_socket.recv(1024)
-                if not data:
-                    break
-                print(f"Received: {data}")
-                client_socket.sendall(data)  # Echo the received data
-        except socket.error:
-            break
+def message_received(client, server, message):
+    print(f"Received message from {client['address']}: {message}")
+    server.send_message(client, message)
 
-    server_socket.close()
-    with lock:
-        del servers[(host, port)]
-    print(f"Socket server on {host} port {port} stopped")
+def start_websocket_server():
+    host = '127.0.0.1'
+    server = WebsocketServer(port=0, host=host)
+    server.set_fn_new_client(new_client)  # 새로운 클라이언트 연결 콜백 설정
+    server.set_fn_message_received(message_received)
 
-def start_socket_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 0))  # Bind to a free port
-    server_socket.listen(1)
-    host, port = server_socket.getsockname()
-    threading.Thread(target=socket_server_thread, args=(server_socket, host, port)).start()
-
-def handle_request():
-    start_socket_server()
+    actual_port = server.server_address[1]  # 실제 할당된 포트 번호
+    servers[(host, actual_port)] = server
+    server.run_forever()
 
 @app.route('/start')
 def start():
-    thread = threading.Thread(target=handle_request)
+    thread = threading.Thread(target=start_websocket_server)
     thread.start()
-    time.sleep(1)  # Delay to ensure the server starts
-    with lock:
-        host, port = list(servers.keys())[-1]
-    return jsonify({"message": "Server is starting", "host": host, "port": port}), 200
+    time.sleep(1)
+    host, port = list(servers.keys())[-1]
+    return jsonify({"message": "WebSocket server started", "host": host, "port": port}), 200
 
-@app.route('/stop', methods=['POST'])
+@app.route('/stop', methods=['DELETE'])
 def stop():
     data = request.json
     host, port = data['host'], int(data['port'])
-    with lock:
-        if (host, port) in servers and servers[(host, port)]["active"]:
-            servers[(host, port)]["active"] = False
-            return jsonify({"message": "Server stopping"}), 200
-        else:
-            return jsonify({"error": "Server not found"}), 404
 
-@app.route('/list')
-def list_servers():
-    with lock:
-        active_servers = [{"host": host, "port": port} for (host, port), server in servers.items() if server["active"]]
-    return jsonify(active_servers), 200
+    if (host, port) in servers:
+        server = servers[(host, port)]
+        server.server_close()
+        del servers[(host, port)]
+        return jsonify({"message": "WebSocket server stopped"}), 200
+    else:
+        return jsonify({"error": "Server not found"}), 404
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
